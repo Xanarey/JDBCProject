474b3fc4779ad688837136ae3f8aa71953d20905
Xanarey <doctor.layout@mail.ru>
2022-08-16 13:18:29 +0300

    test_developer

---
+++ src/test/java/com/service/DeveloperServiceTest.java
@@ -0,0 +1,35 @@
+package com.service;
+
+import com.repository.JdbcImpl.JdbcDeveloperRepositoryImpl;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.junit.Assert.*;
+
+public class DeveloperServiceTest {
+
+    DeveloperService developerService;
+
+    @Mock
+    JdbcDeveloperRepositoryImpl jdbcDeveloperRepository;
+
+    @Test
+    public void getById() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void deleteById() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void insert() {
+    }
+}
