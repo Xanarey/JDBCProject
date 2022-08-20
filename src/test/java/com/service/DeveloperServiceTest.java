ae75202aec20442e50bc2a9f4cb9e22c574cf6f6
Xanarey <doctor.layout@mail.ru>
2022-08-20 10:36:10 +0300

    tests

--- src/test/java/com/service/DeveloperServiceTest.java
+++ src/test/java/com/service/DeveloperServiceTest.java
@@ -1,6 +1,9 @@
 package com.service;
 
+import com.controller.DeveloperController;
 import com.model.Developer;
+import com.model.Specialty;
+import com.model.Status;
 import com.repository.JdbcImpl.JdbcDeveloperRepositoryImpl;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,15 +19,24 @@ public class DeveloperServiceTest {
 
     private final DeveloperService developerService = new DeveloperService();
     private final JdbcDeveloperRepositoryImpl jdri = new JdbcDeveloperRepositoryImpl();
+    private final DeveloperController dc = new DeveloperController();
+    Specialty specialty = new Specialty();
     List<Developer> developerList;
+    Developer updateDeveloper;
+    Developer deletedDeveloper;
+    Developer developer;
 
     @Before
     public void setUp() {
+        specialty.setId(1L);
         developerList = jdri.getAll();
+        developer = jdri.getById(1L);
+        updateDeveloper = dc.updateDeveloper(1L, "A", "B", Status.ACTIVE, specialty);
     }
 
     @Test
     public void getById() {
+        assertEquals(developerService.getById(1L), developer);
     }
 
     @Test
@@ -35,10 +47,12 @@ public class DeveloperServiceTest {
 
     @Test
     public void deleteById() {
+
     }
 
     @Test
     public void update() {
+        assertEquals(dc.updateDeveloper(1L, "A", "B", Status.ACTIVE, specialty), jdri.update(updateDeveloper));
     }
 
     @Test
