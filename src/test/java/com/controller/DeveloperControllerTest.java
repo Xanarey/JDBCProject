9a3e34131d8e12ba98c557b33e5f51f71e0d23fd
Xanarey <doctor.layout@mail.ru>
2022-08-25 16:12:01 +0300

    other

---
+++ src/test/java/com/controller/DeveloperControllerTest.java
@@ -0,0 +1,34 @@
+package com.controller;
+
+import com.model.Developer;
+import com.service.DeveloperService;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DeveloperControllerTest {
+
+    private final DeveloperController developerController = new DeveloperController();
+    private final DeveloperService developerService = new DeveloperService();
+
+    @Test
+    public void getDeveloperById() {
+
+    }
+
+    @Test
+    public void getAllDeveloper() {
+    }
+
+    @Test
+    public void deleteDeveloperById() {
+    }
+
+    @Test
+    public void updateDeveloper() {
+    }
+
+    @Test
+    public void insertDeveloper() {
+    }
+}
